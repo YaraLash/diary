70c3fe3eab81a7933a4b9a9b430be8c14371056b
YaraLash <94112400+YaraLash@users.noreply.github.com>
2022-05-11 19:41:39 +0700

    add file of tracing types

---
+++ src/utils/types/index.ts
@@ -0,0 +1,9 @@
+import {WeekType} from "./diary/week.type";
+import {DayType} from "./diary/day.type";
+import {SubjectType} from "./diary/subject.type";
+
+export type {
+    WeekType,
+    DayType,
+    SubjectType
+}
